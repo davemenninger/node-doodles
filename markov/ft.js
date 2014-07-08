f73aba8f1efc2dd029f3cfe14912067c2d85160f
Dave Menninger <dave.menninger@gmail.com>
2014-07-07 20:51:59 -0400

    different source for markov chain

--- markov/ft.js
+++ markov/ft.js
@@ -7,6 +7,7 @@ var stdin = process.openStdin();
 stdin.addListener("data", function(d) {
     var word = d.toString().substring(0, d.length-1);
     console.log("you entered: [" + word + "]");
-    var m = new MarkovChain({ files: '503-oneliner' });
-    m.start( word ).end( 5 ).process( function(err,s){ console.log(s) } );
+    //var m = new MarkovChain({ files: '503-oneliner' });
+    var m = new MarkovChain({ files: 'pg8492-oneliner.txt' });
+    m.start( word ).end( 10 ).process( function(err,s){ console.log(s) } );
   });
