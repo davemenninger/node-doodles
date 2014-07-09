e3d73a6f88d01e179a9486855a066c110d268551
Dave Menninger <dave.menninger@gmail.com>
2014-07-08 22:07:35 -0400

    shows the character followed by the name

--- emoji/em2.js
+++ emoji/em2.js
@@ -1,3 +1,10 @@
 var emoji = require( "emoji" );
 
-console.log( Object.keys( emoji.EMOJI_MAP ).join(" ") );
+//console.log( Object.keys( emoji.EMOJI_MAP ).join(" ") );
+
+var emojis = Object.keys( emoji.EMOJI_MAP );
+
+for ( var i = 0; i < emojis.length; i++ )
+{
+  console.log( emojis[i] + " " + emoji.EMOJI_MAP[emojis[i]][1] );
+}
