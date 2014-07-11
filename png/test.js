a48cbf2f82e034bceabf80a5977d86b20a4420b5
Dave Menninger <dave.menninger@gmail.com>
2014-07-11 14:16:02 -0400

    build png out of array of numbers

--- png/test.js
+++ png/test.js
@@ -1,24 +1,35 @@
 var fs = require('fs'),
     PNG = require('pngjs').PNG;
 
-var i = new PNG({
-  width: 100,
-  height: 200,
+var pixel_array = [
+                [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+                [ 0, 0, 1, 0, 0, 1, 0, 0 ],
+                [ 0, 0, 1, 0, 0, 1, 0, 0 ],
+                [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+                [ 0, 0, 0, 0, 0, 0, 0, 0 ],
+                [ 0, 1, 0, 0, 0, 0, 1, 0 ],
+                [ 0, 0, 1, 1, 1, 1, 0, 0 ],
+                [ 0, 0, 0, 0, 0, 0, 0, 0 ]
+                ];
+
+var png = new PNG({
+  height: pixel_array.length,
+  width:  pixel_array[0].length,
   filterType: -1
 });
 
-for (var y = 0; y < i.height; y++) {
-  for (var x = 0; x < i.width; x++) {
-    var idx = ( i.width*y + x) << 2;
+for (var y = 0; y < png.height; y++) {
+  for (var x = 0; x < png.width; x++) {
+    var idx = ( png.width*y + x) << 2;
 
-    var col = ( Math.floor(Math.random()*2) == 0 ) ? 0x20 : 0xff;
+    var col = ( pixel_array[y][x] == 0 ) ? 0x20 : 0xff;
 
-    i.data[idx ] = col;   //red
-    i.data[idx+1] = col;  //green
-    i.data[idx+2] = col;  //blue
-    i.data[idx+3] = 0xff; //opacity
+    png.data[idx ] = col;   //red
+    png.data[idx+1] = col;  //green
+    png.data[idx+2] = col;  //blue
+    png.data[idx+3] = 0xff; //opacity
   }
 }
 
 
-i.pack().pipe(fs.createWriteStream('out.png'));
+png.pack().pipe(fs.createWriteStream('out.png'));
