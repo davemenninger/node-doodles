4f9d8acbf2ed4cb56d935ed64957847321ef297f
Dave Menninger <dave.menninger@gmail.com>
2014-07-15 21:02:20 -0400

    comments

--- doodle_ebooks.js
+++ doodle_ebooks.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 
+// this express stuff is just to make this thing work on heroku or what have you
 var express = require('express');
 var app = express();
 app.get('/', function(req, res){
     res.send('Hello world.');
 });
-//app.listen(3000);
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
