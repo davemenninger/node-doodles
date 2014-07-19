53398b21e18a25014b1e26a6fcb31b7259648ada
Dave Menninger <dave.menninger@gmail.com>
2014-07-18 20:29:05 -0400

    clearer names

--- ca/ca2.js
+++ ca/ca2.js
@@ -1,26 +1,14 @@
 var fs = require('fs');
 var PNG = require('pngjs').PNG;
 
-function rule(v,r){
+function calc(v,r){
   //calculates the next value of a cell given the neighbors in 3-cell array v, and using rule r (decimal number)
   var left   = v[0] || 0;
   var center = v[1] || 0;
   var right  = v[2] || 0;
-
-  // n is the value of the three neighbors as a decimal number, thus which 3-pattern to match in the rule; e.g. 011 is 3
   var n =  ( parseInt(left,2)*4 + parseInt(center,2)*2 + parseInt(right,2)*1 );  
-  // p = 2^n is a decimal number that in binary has a 1 in the n-th position (right-to-left); e.g. 2^3=8 and 8 = 00001000
   var p = Math.pow(2,n);  
-  // (r&p) is logical and of the rule and p to get the value of the binary digit in the rule at the n-th position (right-to-left);
-  // e.g.: decimal 30, aka rule 30, in binary is 00011110 so:
-  //      00011110 <- the rule
-  // AND  00001000 <- which digit we want
-  // -------------
-  //      00001000 <- will either have a single 1 or be all 0's
-  // which is 8 (2^n) again, so divide by p to get 1.
-  // or if the r&p was 0, then dividing by p just gives 0.
   var result = (r&p)/p ;  
-  
   return result;
 }
 
@@ -36,25 +24,23 @@ function e(s,t,r,filename){
     filterType: -1
   });
 
-  var col1 = (Math.floor(Math.random()*256));
-  var col2 = (Math.floor(Math.random()*256));
-  var col3 = (Math.floor(Math.random()*256));
-  var col4 = (Math.floor(Math.random()*256));
-  col4 = Math.floor( 256 - (col1+col2+col3)/3 );
-      
+  //random colors
+  var red =   (Math.floor(Math.random()*256));
+  var green = (Math.floor(Math.random()*256));
+  var blue =  (Math.floor(Math.random()*256));
+  var bg = Math.floor( 256 - (red+green+blue)/3 );
 
   var n = s.slice(0);
   for (h = 0; h < t; h++) {
     for (i = 0; i < s.length; i++) { 
-      n[i] = rule( [ s[i-1], s[i], s[i+1] ], r );
+      n[i] = calc( [ s[i-1], s[i], s[i+1] ], r );
 
       var x=i;
       var y=h;
       var idx = ( s.length*y + x ) << 2;
-      var col = ( n[i] == 0 ) ? 0x20 : 0xff;
-      png.data[idx]= ( n[i] == 0 ) ? col1 : col4 ;
-      png.data[idx+1]= ( n[i] == 0 ) ? col2 : col4 ;
-      png.data[idx+2]= ( n[i] == 0 ) ? col3 : col4 ;
+      png.data[idx]=   ( n[i] == 0 ) ? red   : bg ;
+      png.data[idx+1]= ( n[i] == 0 ) ? green : bg ;
+      png.data[idx+2]= ( n[i] == 0 ) ? blue  : bg ;
       png.data[idx+3]= 0xff;
 
     }
@@ -69,7 +55,8 @@ function e(s,t,r,filename){
 var a = [];
 var b = [];
 
-var width = 100;
+var width = 600;
+var height = 370;
 
 for ( i = 0; i < width; i++ ){
   a[i] = 0;
@@ -78,6 +65,9 @@ for ( i = 0; i < width; i++ ){
 
 a[width/2] = 1;
 
-e(a,200,30,'foo.png');
-e(b,100,106,'bar.png');
+
+var rule = Math.floor( Math.random()*256 );
+
+e(a,height,rule,'foo.png');
+e(b,height,rule,'bar.png');
 
